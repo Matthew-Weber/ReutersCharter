7f943ce7ce43782b1494b325d97ad172e0683a23
Matthew Weber <matthew.weber@thomsonreuters.com>
2019-05-02 10:53:21 -0400

    adds map charter

--- scripts/ReutersCharter.js
+++ scripts/ReutersCharter.js
@@ -3,13 +3,15 @@ import { BarChart } from './BarChart.js'
 import { ScatterChart } from './ScatterChart.js'
 import { DataStreamParse } from './DataStreamParse.js'
 import { BespokeBase } from './BespokeBase.js'
+import { MapCharter } from './MapCharter.js'
 
 let ReutersCharter = {
 	LineChart:LineChart,
 	BarChart:BarChart,
 	DataStreamParse:DataStreamParse,
 	ScatterChart:ScatterChart,
-	BespokeBase:BespokeBase
+	BespokeBase:BespokeBase,
+	MapCharter:MapCharter
 }
 
 export default ReutersCharter
